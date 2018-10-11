28b5bf4f2f8d5c56d15c73a2a79ef01df508bb19
Thomas Braun <braunthomas004@gmail.com>
2018-10-10 21:36:58 -0400

    testing commit

---
+++ lab004/Controllers/Lab4Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab004.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["date1"] = DateTime.Now.ToShortTimeString();
+            ViewData["date2"] = DateTime.Now.ToLongDateString();
+            DateTime now = DateTime.Now;
+            DateTime end = new DateTime(now.Year + 1, 1, 1);
+            int daysLeftInYear = (int)(end - now).TotalDays;
+            ViewData["date3"] = daysLeftInYear;
+            ViewData["year1"] = now.Year;
+            ViewData["testing"] = DateTime.Now.Hour;
+            if (DateTime.Now.Hour < 12 ) { ViewData["greeting"] = "Good Morning"; }
+            else if (DateTime.Now.Hour > 18) { ViewData["greeting"] = "Good Evening"; }
+            else { ViewData["greeting"] = "Good Afternoon"; }
+            return View();
+        }
+        public IActionResult Waffles()
+        {
+            return View();
+        }
+    }
+}
